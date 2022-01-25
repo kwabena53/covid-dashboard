45684ff2a878e3090f1fee869e19591bdc9ee9d5
Kwabena Adu-Darkwa <kwabenaadudarkwa@gmail.com>
2022-01-25 12:20:10 -0700

    fixed error

--- app.js
+++ app.js
@@ -15,8 +15,8 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'hbs');
 
 const host = '0.0.0.0';
-app.listen(process.env.PORT || 3000, ()=> {
-    console.log("App started at port 80" );
+app.listen(process.env.PORT || 8000, ()=> {
+    console.log("App started at port 8000" );
 });
 
 app.use(logger('dev'));
